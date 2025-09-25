d1e980d96e8cb8c0c2ec65f3db21d72e8fc201a6
Agrowise <Agrowise.app@gmail.com>
2025-09-25 18:35:13 +0300

    Agro - Admin : For railway deploy (Urls)

--- Agro-admin/src/api/settings/returnUrl.js
+++ Agro-admin/src/api/settings/returnUrl.js
@@ -1,14 +1,15 @@
-const Use_Localhost_BaseUrl = true;
+const Use_Localhost_BaseUrl = false;
 
 export const returnUrl = (url) => {
   //console.log("called");
   if (Use_Localhost_BaseUrl) {
     url = "http://localhost:3001";
     //url = "http://192.168.2.7:3001";
-    //url = "https://agrowise-application.onrender.com";
     //url = "http://192.168.1.77:3001";
     //url = "http://192.168.1.178:3001";
     //url = "http://192.168.1.177:3001";
+  } else {
+    url = "https://agrowise-production.up.railway.app";
   }
   return url;
 };
